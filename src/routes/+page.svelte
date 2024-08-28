80c349ff70f4528e5aeca89014a25946c6f3819f
FuryForev3r <messiasfuryforever@gmail.com>
2024-08-28 14:20:57 -0300

    feat(explorer): Close button added fix(explorer): Project path system

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="container">
-    <Explorer projectPath={projectPath} />
+    <Explorer />
     <div class:codeMargin={$editorOpenPath}>
         <Tablist />
         {#if $editorOpenPath}
